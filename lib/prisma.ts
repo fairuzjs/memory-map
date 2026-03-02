cc45a7e90ef1618eaa8bbec136b9d2ab7ae2b5b2
draafairuzz <draatzy37@gmail.com>
2026-03-02 13:49:45 +0700

    remove prisma adapter (v7 config)

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,12 @@
-import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from "@prisma/client"
 
-const connectionString = process.env.DATABASE_URL
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ adapter })
+  globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
